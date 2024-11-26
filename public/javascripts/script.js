cf7332240e1209fd9c052be5f12b1af20c604588
Abderahim <ita27rmp100@gmail.com>
2024-11-26 08:23:53 +0100

    changing in the navbar

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,18 @@
 if ($('#pw').text()!=$('#ConfirmPW').text()) {
     alert('Confirm your password please.')
 }
+let width_height = [
+    {
+        width:"350px",
+        height:"225px"
+    },
+    {
+        width:"50px",
+        height:"50px"
+    }
+]
+let change_mode = 0
+function spread(){
+    $("header").css(width_height[change_mode])
+    change_mode = Math.abs(change_mode-1)
+}
